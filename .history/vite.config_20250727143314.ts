c511a0857999990f0354729747335a5bb452a62f
Wipas1919 <wipas2526@gmail.com>
2025-07-27 15:04:39 +0700

    Add SPA support for GitHub Pages in index.html and set base path in vite.config.ts

--- .history/vite.config_20250727143314.ts
+++ .history/vite.config_20250727143314.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio-wipas/", // เพิ่มบรรทัดนี้
   server: {
     host: "::",
     port: 8080,
